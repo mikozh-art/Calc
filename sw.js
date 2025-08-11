b71e32080ff273438164442c4f12e65398853658
mikozh-art <mzhmuruk@gmail.com>
2025-08-11 18:39:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
+const CACHE_NAME = 'calc-cache-v1';
+const BASE = '/Calc/';
+const ASSETS = [
+  BASE,
+  BASE + 'index.html',
+  BASE + 'manifest.json',
+  BASE + 'icon/icon-192.png',
+  BASE + 'icon/icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js'
-      ]);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
